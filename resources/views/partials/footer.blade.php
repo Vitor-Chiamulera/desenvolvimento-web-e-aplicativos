79a2de4af7d8befde592590784fd57115c095b7e
Vitor Chiamulera <vitorchiamulera@gmail.com>
2026-05-01 23:12:31 -0300

    feat: adiciona estrutura base de layout e estilos

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,3 @@
+<nav>
+    <a href="/">teste</a>
+</nav>
---
+++ resources/views/partials/navbar.blade.php
@@ -0,0 +1,14 @@
+<div class="navbar__container">
+    <nav class="main_navbar">
+        <div class="nav_icon">
+            <a href="/">Icon</a>
+        </div>
+        <div class="nav_middle">
+            <a href="/about">Sobre</a>
+            <a href="/cadastro/livro">Cadastrar Livro</a>
+        </div>
+        <div class="nav_end">
+            <a href="/login">Login</a>
+        </div>
+    </nav>
+</div>
